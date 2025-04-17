9c278b26b9aed770390c061dbf985e3ce8270cd6
SEOJIAF <dhlavaty.jr@gmail.com>
2025-04-17 21:50:29 +0200

    feat: update popup styling and add QR code button for enhanced user interaction

--- my-app/src/routes/load/+page.svelte
+++ my-app/src/routes/load/+page.svelte
@@ -87,26 +87,26 @@
     <button on:click={() => goto("/")}>Back</button>
     <p>Retrieved Text</p>
     <button on:click={copytext} class="copyBtn">{copybutton}</button>
-    
+    <button on:click={togglePopup} class="copyBtn">Qr code</button>
     <!-- <button on:click={makeQr} class="copyBtn">Qr code</button> -->
     <pre class="retrieved">{retrievedText}</pre>
 
     
-    <button on:click={togglePopup}>Show Popup</button>
+
+
 
     {#if showPopup}
-      <div class="popup-overlay" role="button" tabindex="0" on:click={togglePopup} on:keydown={(e) => e.key === 'Enter' && togglePopup()}>
-        <!-- svelte-ignore a11y_click_events_have_key_events -->
-        <!-- svelte-ignore a11y_interactive_supports_focus -->
-        <div class="popup-content" role="dialog" aria-modal="true" on:click|stopPropagation>
-          <h2>Hello!</h2>
-          <p>This is a popup window.</p>
-          <img src={qrUrl} alt="s"class="qr">
-          <p></p>
-          <button class="close-button" on:click={togglePopup}>Close</button>
-        </div>
+    <div class="popup-overlay" role="button" tabindex="0" on:click={togglePopup} on:keydown={(e) => e.key === 'Enter' && togglePopup()}>
+      <!-- svelte-ignore a11y_click_events_have_key_events -->
+      <!-- svelte-ignore a11y_interactive_supports_focus -->
+      <div class="popup-content" role="dialog" aria-modal="true" on:click|stopPropagation>
+        <h2>Hello!</h2>
+        <p>This is a popup window.</p>
+        <img src={qrUrl} alt="s"class="qr">
+        <p></p>
+        <button class="close-button" on:click={togglePopup}>Close</button>
       </div>
+    </div>
     {/if}
-
   </main>
   
