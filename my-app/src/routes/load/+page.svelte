b531e3a32a304135a4ba2a3ae414bfa73c146b29
SEOJIAF <dhlavaty.jr@gmail.com>
2025-04-18 11:55:38 +0200

    feat: increase size of back navigation arrow for better visibility

--- my-app/src/routes/load/+page.svelte
+++ my-app/src/routes/load/+page.svelte
@@ -79,7 +79,7 @@
   <div class="arrow">
     <!-- svelte-ignore a11y_click_events_have_key_events -->
     <!-- svelte-ignore a11y_no_static_element_interactions -->
-    <svg  class="goback"on:click={() => goto('/')} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+    <svg  class="goback"on:click={() => goto('/')} xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
     </div>
   <!-- svelte-ignore a11y_click_events_have_key_events -->
   <!-- svelte-ignore a11y_no_static_element_interactions -->
